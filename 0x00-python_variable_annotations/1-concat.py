568ddbbedeb6b502e318d5f984d6c06fe9da4fde
Uchenna Oko <uche4newton1981@gmail.com>
2024-03-15 19:42:39 +0100

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings together and returns the result.
+
+    Parameters:
+        str1 (str): The first string.
+        str2 (str): The second string.
+
+    Returns:
+        str: The concatenation of str1 and str2.
+    """
+    return str1 + str2
