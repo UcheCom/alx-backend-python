5f0b48fa482109998360f0c862e121de1992e310
Uchenna Oko <uche4newton1981@gmail.com>
2024-03-15 19:42:01 +0100

    Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+def add(a: float, b: float) -> float:
+    """
+    This adds two floats together and returns their sum.
+
+    Parameters:
+        a (float): The first number.
+        b (float): The second number.
+
+    Returns:
+        float: The sum of a and b.
+    """
+    return a + b
