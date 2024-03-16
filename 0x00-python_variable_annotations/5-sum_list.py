d1a0da50a38620baee29a31facc4c21b5bea1d0f
Uchenna Oko <uche4newton1981@gmail.com>
2024-03-16 06:02:40 +0100

    Complex types - list of floats

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -9,4 +9,7 @@ def sum_list(input_list: List[float]) -> float:
     Parameters:
         input_list (list[float]): The list of floats.
     """
-    return sum(input_list)
+    if input_list is None:
+        return 0
+    else:
+        return sum(input_list)
