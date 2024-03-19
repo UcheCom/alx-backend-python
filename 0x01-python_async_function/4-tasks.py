13290ed095e2b0be0b75ce4531b68ed0204c8ab8
Uchenna Oko <uche4newton1981@gmail.com>
2024-03-19 16:04:01 +0100

    Task 4

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module takes the code from wait_n and alter
+it into a new function task_wait_n
+"""
+import asyncio
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n, max_delay) -> List[float]:
+    """This spawns wait_random n times
+    Return: List of delays
+    """
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    return [await task for task in asyncio.as_completed(tasks)]
