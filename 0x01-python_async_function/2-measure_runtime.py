5a406c7fe233edb0a5bf8a1c08fb1be37d33ff79
Uchenna Oko <uche4newton1981@gmail.com>
2024-03-19 16:24:23 +0100

    Measure the runtime error fixed

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -7,7 +7,7 @@ from time import perf_counter
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-def measure_time(n, max_delay) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """This measures the runtime
     Args:
         n: the number of coroutines to launch
